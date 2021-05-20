28383941e06e118166db83a4e982bf8d7df7f462
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-05-20 14:49:10 -0500

    NODE: Creando la lógica para el manejo d las tareas por hacer

--- tareas-consola/app.js
+++ tareas-consola/app.js
@@ -1,7 +1,9 @@
 
 require('colors');
 
-const { inquirerMenu } = require('./helper/inquirer');
+const { inquirerMenu, pausa } = require('./helper/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
 
 console.clear();
@@ -12,9 +14,16 @@ const main = async () => {
 
     do {
 
-        opt = await inquirerMenu();
-        console.log({ opt });
+        /* opt = await inquirerMenu();
+        console.log({ opt }); */
 
+        const tareas  = new Tareas();
+         const tarea = new Tarea('Comprar comida');
+        tareas._listado[tareas] = tarea;
+
+         console.log(tareas);
+
+        await pausa();
         /* if (opt !== 0) await pausa(); */
 
     } while (opt !== '0');
--- tareas-consola/helper/inquirer.js
+++ tareas-consola/helper/inquirer.js
@@ -7,10 +7,47 @@ const menuOpts = [
     {
         type: 'list',
         name: 'opcion',
-        message: '¿Qué desea hacer?',
-        choices: ['opt1', 'opt2', 'opt3']
+        message: '¿Qué desea hacer?\n',
+        choices: [
+            {
+                value: '1',
+                name: '1. Crear una tarea'
+            },
+            {
+                value: '2',
+                name: '2. Listar tareas'
+            },
+            {
+                value: '3',
+                name: '3. Listar tareas completadas'
+            }
+            ,
+            {
+                value: '4',
+                name: '4. Listar tareas pendientes'
+            }
+            ,
+            {
+                value: '5',
+                name: '5. Completar tarea'
+            }
+            ,
+            {
+                value: '6',
+                name: '6. Borrar tarea'
+            },
+            {
+                value: '0',
+                name: '0. Salir'
+            }
+
+        ]
     }
 ];
+
+
+
+
 
 const inquirerMenu = async () => {
     console.clear();
@@ -18,13 +55,27 @@ const inquirerMenu = async () => {
     console.log('** Seleccione una opción **'.yellow);
     console.log('===========================\n'.green);
 
-    const opt = await inquirer.prompt(menuOpts);
+    const { opcion } = await inquirer.prompt(menuOpts);
 
-    return opt;
+    return opcion;
 }
 
+const pausa = async () => {
 
+    const question = [
+        {
+            type: 'input',
+            name: 'enter',
+            message: `Presione ${'enter'.green} para continuar`
+        }
+    ]
+
+    console.log('\n');
+
+    await inquirer.prompt(question);
+}
 
 module.exports = {
-    inquirerMenu
+    inquirerMenu,
+    pausa
 }
